574374e7e32cc6c666e5b2c26ecd84544d33bd6d
Francisco Mercado <sissyyayle0517@hotmail.com>
2021-10-12 18:48:40 -0500

    deleting loggers

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -1,165 +1,174 @@
-const { Router }= require('express')
-const axios= require('axios')
-const router= Router()
-const { Pokemon, Type }= require('../db')
-// const e = require('express')
+const { Router } = require("express");
+const axios = require("axios");
+const router = Router();
+const { Pokemon, Type } = require("../db");
 
 //Obtenemos los pokemons de la API
-const getPokeAPI= async ()=> {
-  
+const getPokeAPI = async () => {
   try {
-    const firstPokemons= await axios.get('http://pokeapi.co/api/v2/pokemon',{
-      timeout:90000,
-    }) //Lo utilizamos para traer los primeros pokemones//
-    const secondPokemons= await axios.get(firstPokemons.data.next) //y resolvemos los siguientes pokemones
-    const totalPokemons= firstPokemons.data.results.concat(secondPokemons.data.results) //unimos el resultado de las peticiones en una variable
-    const pokeUrl= totalPokemons.map(el=> axios.get(el.url)) //Obtengo la url con la info de cada pokemon
-    let pokeInfo= Promise.all(pokeUrl) //Paso en un arreglo de promesas con la respuesta de cada url con la información.
-      .then(el=> {
-        let pokemon= el.map(e=> e.data) //Obtengo la data de cada pokemon
-        let info=[] //Creo un array vacio para guardar la data de cada pokemon
-        pokemon.map(el=> {
+    const firstPokemons = await axios.get("http://pokeapi.co/api/v2/pokemon", {
+      timeout: 90000,
+    }); //Lo utilizamos para traer los primeros pokemones//
+    const secondPokemons = await axios.get(firstPokemons.data.next); //y resolvemos los siguientes pokemones
+    const totalPokemons = firstPokemons.data.results.concat(
+      secondPokemons.data.results
+    ); //unimos el resultado de las peticiones en una variable
+    const pokeUrl = totalPokemons.map((el) => axios.get(el.url)); //Obtengo la url con la info de cada pokemon
+    let pokeInfo = Promise.all(pokeUrl) //Paso en un arreglo de promesas con la respuesta de cada url con la información.
+      .then((el) => {
+        let pokemon = el.map((e) => e.data); //Obtengo la data de cada pokemon
+        let info = []; //Creo un array vacio para guardar la data de cada pokemon
+        pokemon.map((el) => {
           //Guardamos la info del pokemon en el array...
           info.push({
             id: el.id,
-            name:el.name,
+            name: el.name,
             hp: el.stats[0].base_stat,
-            attack:el.stats[1].base_stat,
+            attack: el.stats[1].base_stat,
             defense: el.stats[2].base_stat,
             speed: el.stats[5].base_stat,
             height: el.height,
-            weight:el.weight,
+            weight: el.weight,
             sprite: el.sprites.other.dream_world.front_default,
-            types: el.types.length < 2 ? [{name: el.types[0].type.name}] : [{name: el.types[0].type.name}, {name:el.types[1].type.name}]
-          })
-        })
-        return info
-      }).catch(error=> console.log(error))
-      return pokeInfo
-  }catch(error){
-    throw error
+            types:
+              el.types.length < 2
+                ? [{ name: el.types[0].type.name }]
+                : [
+                    { name: el.types[0].type.name },
+                    { name: el.types[1].type.name },
+                  ],
+          });
+        });
+        return info;
+      })
+      .catch((error) => console.log(error));
+    return pokeInfo;
+  } catch (error) {
+    throw error;
   }
-  }
+};
 //Obtenemos los datos de los pokemon en la base de datos
-const getPokeDb= async()=> {
+const getPokeDb = async () => {
   try {
     return await Pokemon.findAll({
       include: {
         model: Type,
-        attributes:['name'],
+        attributes: ["name"],
         through: {
           attributes: [],
         },
-      }
-    })
-  } catch(error) {
-    console.error('Something went wrong')
-    console.error(error)
+      },
+    });
+  } catch (error) {
+    console.error("Something went wrong");
+    console.error(error);
   }
-}
+};
 
 // Obtenemos la imagen de un pokemon al crearlo...
 
-const getNameUrl= async (name) => {
+const getNameUrl = async (name) => {
   try {
-    const pokeUrl= await axios.get(`http://pokeapi.co/api/v2/pokemon/${name}`,{
-      timeout: 90000,
-    })
+    const pokeUrl = await axios.get(`http://pokeapi.co/api/v2/pokemon/${name}`);
     return pokeUrl.data.sprites.other.dream_world.front_default;
   } catch (error) {
-    console.error("me lleva!!!", error)
+    console.error("me lleva!!!", error);
   }
-}
+};
 
 //Unimos los datos de la API con la base de datos
 
-const allPokemons= async ()=> {
-  const api= await getPokeAPI()
-  const db= await getPokeDb()
-  const result= api ? api.concat(db) : ''
-  return result
-}
+const allPokemons = async () => {
+  const api = await getPokeAPI();
+  const db = await getPokeDb();
+  const result = api ? api.concat(db) : "";
+  return result;
+};
 
-let pokeArr= [];
+let pokeCache = []; //creamos una memoria cache para evitar errores al cargar pokemones...
 
 // Routes
 
-router.get('/', async (req,res)=> {
-  const { name }= req.query
-  
-  console.log('pokeArr es: ' + pokeArr.length)
-  
+router.get("/", async (req, res) => {
+  const { name } = req.query;
+
   try {
-    const pokeTotal= await allPokemons()
-    pokeTotal.length > pokeArr.length ? pokeArr= pokeTotal.slice(0,pokeTotal.length) : pokeArr
-    console.log('PokeTotal: ' + pokeTotal.length)
-    console.log('PokeArr: ' + pokeArr.length)
-    if(name) {
-      let pokeName= await pokeArr.find(el=> el.name.toLowerCase() === name.toLowerCase())
-      if(pokeName !== undefined) {
-        console.log(pokeName)
-        return res.status(200).json(pokeName)
-      }else {
-        return res.status(201).json({error: true})
+    const pokeTotal = await allPokemons();
+    //verificamos si hay algun pokemon nuevo...
+    pokeTotal.length > pokeCache.length
+      ? (pokeCache = pokeTotal.slice(0, pokeTotal.length))
+      : pokeCache;
+
+    if (name) {
+      let pokeName = await pokeCache.find(
+        (el) => el.name.toLowerCase() === name.toLowerCase()
+      );
+      if (pokeName !== undefined) {
+        console.log(pokeName);
+        return res.status(200).json(pokeName);
+      } else {
+        return res.status(201).json({ error: true });
       }
     } else {
-      return res.status(202).json(pokeArr)
+      return res.status(202).json(pokeCache);
     }
-  } catch(error) {
-    return res.status(404).send('El get/home valió...');
+  } catch (error) {
+    return res.status(404).send("El get/home valió...");
   }
-})
+});
 
-
-
-
-router.get('/:id', async (req,res)=> {
-  const {id}= req.params
-  // console.log(id.length)
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
 
   try {
     let pokeTotal;
-    if(id.length > 9) {
-      pokeId= await getPokeDb()
-      pokeTotal= pokeId.find(el=> el.id === id)
-      // console.log(pokeTotal)
-    }else {
-      // pokeTotal= await getPokeAPI()
-      pokeTotal= pokeArr.find(el=> el.id === parseInt(id))
-      // console.log(pokeTotal)
+    if (id.length > 9) {
+      pokeId = await getPokeDb();
+      pokeTotal = pokeId.find((el) => el.id === id);
+    } else {
+      pokeTotal = pokeCache.find((el) => el.id === parseInt(id));
     }
-    if(id) {
-      let pokeId= isNaN(pokeTotal) || pokeTotal.length > 0 ? pokeTotal : []  ; 
-      res.status(200).json(pokeId)
-    }else{
-      res.status(404).send('error')
+    if (id) {
+      let pokeId = isNaN(pokeTotal) || pokeTotal.length > 0 ? pokeTotal : [];
+      res.status(200).json(pokeId);
+    } else {
+      res.status(404).send("error");
     }
-  } catch(error) {
-    throw error
+  } catch (error) {
+    throw error;
   }
-})
+});
 
-router.get('/search/:name', async (req,res)=> {
-  const {name}= req.params
-  console.log(name)
-  const pokeUrlData= await getNameUrl(name)
+router.get("/search/:name", async (req, res) => {
+  const { name } = req.params;
+  const pokeUrlData = await getNameUrl(name);
   try {
-    if(name) {
-      return res.status(200).json(pokeUrlData)
-    }else {
-      return res.status(400).send('No cargo esa cosa...')
+    if (name) {
+      return res.status(200).json(pokeUrlData);
+    } else {
+      return res.status(400).send("No cargo esa cosa...");
     }
   } catch (err) {
-    res.status(400).send('No good!!...', err)
+    res.status(400).send("No good!!...", err);
   }
-})
+});
 
-
-router.post('/', async (req, res)=> {
-  const { name, hp, attack, defense, speed, height, weight, sprite, createInDb, types}= req.body
+router.post("/", async (req, res) => {
+  const {
+    name,
+    hp,
+    attack,
+    defense,
+    speed,
+    height,
+    weight,
+    sprite,
+    createInDb,
+    types,
+  } = req.body;
 
   try {
-    if(name) {
+    if (name) {
       const createPokemon = await Pokemon.create({
         name,
         hp,
@@ -169,24 +178,21 @@ router.post('/', async (req, res)=> {
         height,
         weight,
         sprite,
-        createInDb
-      })
-      const createDb= await Type.findAll({
+        createInDb,
+      });
+      const createDb = await Type.findAll({
         where: {
-          name: types
-        }
-      })
-      createPokemon.addType(createDb)
-      return res.status(200).send('Pokemon succefuly created')
-    }else{
-      return res.status(404).send('Pokemon was not created')
+          name: types,
+        },
+      });
+      createPokemon.addType(createDb);
+      return res.status(200).send("Pokemon succefuly created");
+    } else {
+      return res.status(404).send("Pokemon was not created");
     }
-  }catch(error) {
-    throw error
+  } catch (error) {
+    throw error;
   }
-})
+});
 
-
-
-module.exports= router;
-
+module.exports = router;
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,130 +1,142 @@
 // import { bindActionCreators } from "redux"
 
-const initialState= {
+const initialState = {
   pokemons: [],
   allPokemons: [],
   types: [],
   detail: [],
-  pokemonBackup:0,
-  pokeCache:[],
-  pokeNameData:[],
-  filteredPokemons:[],
+  pokemonBackup: 0,
+  pokeCache: [],
+  pokeNameData: [],
+  filteredPokemons: [],
   searchBackup: false,
-}
+};
 
-function rootReducer(state= initialState, action ) {
-  console.log('llegué...', action.payload)
-  switch(action.type){
-    case 'GET_POKEMONS':
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case "GET_POKEMONS":
       return {
         ...state,
-        pokemons:action.payload,
-        allPokemons:action.payload,
-    }
+        pokemons: action.payload,
+        allPokemons: action.payload,
+      };
 
-    case 'GET_NAME_POKEMONS':
+    case "GET_NAME_POKEMONS":
       return {
         ...state,
-        filteredPokemons:action.payload,
+        filteredPokemons: action.payload,
         // searchBackup: true,
-    }
+      };
 
-    case 'GET_NAME_DATA':
+    case "GET_NAME_DATA":
       return {
         ...state,
-        pokeNameData: action.payload
-      }
+        pokeNameData: action.payload,
+      };
 
-    case 'FILTER_BY_TYPE':
-      const allPokemons= state.allPokemons
-      const filtered= action.payload === 'all' ? allPokemons : allPokemons.filter(e => e.types.map(el=> el.name)[0] === action.payload || e.types.map(el => el.name)[1] === action.payload)
-      if(filtered === 0) return alert('No Pokémons finded...')
+    case "FILTER_BY_TYPE":
+      const allPokemons = state.allPokemons;
+      const filtered =
+        action.payload === "all"
+          ? allPokemons
+          : allPokemons.filter(
+              (e) =>
+                e.types.map((el) => el.name)[0] === action.payload ||
+                e.types.map((el) => el.name)[1] === action.payload
+            );
+      if (filtered === 0) return alert("No Pokémons finded...");
       return {
         ...state,
         filteredPokemons: filtered,
         searchBackup: true,
-    }
+      };
 
-    case 'FILTER_CREATED':
-      const allPokemons2= state.allPokemons
-      const created= action.payload === 'created' ? allPokemons2.filter(e => e.createdInDb)  : allPokemons2.filter(e=> !e.createdInDb)
+    case "FILTER_CREATED":
+      const allPokemons2 = state.allPokemons;
+      const created =
+        action.payload === "created"
+          ? allPokemons2.filter((e) => e.createdInDb)
+          : allPokemons2.filter((e) => !e.createdInDb);
       return {
         ...state,
-        filteredPokemons: action.payload === 'all' ? allPokemons2 : created,
-      }
+        filteredPokemons: action.payload === "all" ? allPokemons2 : created,
+      };
 
-    case 'ORDER_BY_NAME':
-      let sortElement= action.payload === 'asc' ?
-      state.pokemons.sort((a,b)=> {
-        if(a.name > b.name)  return 1
-        if(b.name > a.name)  return -1
-        return 0
-      }) :
-      state.pokemons.sort((a,b)=> {
-        if(a.name > b.name) return -1
-        if(b.name > a.name) return 1
-        return 0
-      })
+    case "ORDER_BY_NAME":
+      let sortElement =
+        action.payload === "asc"
+          ? state.pokemons.sort((a, b) => {
+              if (a.name > b.name) return 1;
+              if (b.name > a.name) return -1;
+              return 0;
+            })
+          : state.pokemons.sort((a, b) => {
+              if (a.name > b.name) return -1;
+              if (b.name > a.name) return 1;
+              return 0;
+            });
       return {
         ...state,
-        pokemons: sortElement
-      }
+        pokemons: sortElement,
+      };
 
-    case 'ORDER_BY_ATTACK':
-      let sortAttack= action.payload === 'strong' ?
-      state.pokemons.sort((a,b)=> {
-        if(a.attack > b.attack) return -1
-        if(b.attack > a.attack) return 1
-        return 0
-      }) :
-      state.pokemons.sort((a,b)=> {
-        if(a.attack > b.attack) return 1
-        if(b.attack > a.attack) return -1
-        return 0
-      })
+    case "ORDER_BY_ATTACK":
+      let sortAttack =
+        action.payload === "strong"
+          ? state.pokemons.sort((a, b) => {
+              if (a.attack > b.attack) return -1;
+              if (b.attack > a.attack) return 1;
+              return 0;
+            })
+          : state.pokemons.sort((a, b) => {
+              if (a.attack > b.attack) return 1;
+              if (b.attack > a.attack) return -1;
+              return 0;
+            });
       return {
         ...state,
-        pokemons: sortAttack
-      }
+        pokemons: sortAttack,
+      };
 
-    case 'POST_POKEMON':
-      return {
-        ...state
-      }
-
-    case 'GET_TYPES':
-      return{
-        ...state,
-        types: action.payload
-      }
-
-    case 'GET_DETAILS':
+    case "POST_POKEMON":
       return {
         ...state,
-        detail: action.payload
-      }
+      };
 
-    case 'GET_INITIAL_STATE':
+    case "GET_TYPES":
+      return {
+        ...state,
+        types: action.payload,
+      };
+
+    case "GET_DETAILS":
+      return {
+        ...state,
+        detail: action.payload,
+      };
+
+    case "GET_INITIAL_STATE":
       return {
         ...state,
         detail: [],
-        filteredPokemons:[],
-        pokeNameData:[],
-      }
-      
-    case 'GET_PAGINATE':
+        filteredPokemons: [],
+        pokeNameData: [],
+      };
+
+    case "GET_PAGINATE":
       return {
         ...state,
-        pokemonBackup: action.payload
-      }
+        pokemonBackup: action.payload,
+      };
 
-    case 'CACHE_API': 
-    return {
-      ...state,
-      pokeCache: action.payload
-    }
+    case "CACHE_API":
+      return {
+        ...state,
+        pokeCache: action.payload,
+      };
 
-    default: return state;
+    default:
+      return state;
   }
 }
 
