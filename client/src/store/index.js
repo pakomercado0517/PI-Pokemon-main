924b3aadd2e31bbf55b4c673b9b149cb5aa767f4
pakomercado0517 <sissyyayle0517@hotmail.com>
2023-11-06 22:38:46 -0600

    new version of pako pokedex

--- client/src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension' 
-import rootReducer from '../reducers'
-import thunk from 'redux-thunk'
-
-
-export const store= createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
